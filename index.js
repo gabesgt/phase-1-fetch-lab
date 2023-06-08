1d1c1acd90a3e965ebaee9148017509edb220251
Gabriel Sargent <gabe2sargent@gmail.com>
2023-06-08 05:13:46 -0500

    fetLAb

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then((json) => renderBooks(json))
+
+
+
+
+
+
+
   // To pass the tests, don't forget to return your fetch!
-  
+
 }
 
 function renderBooks(books) {
+
+
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
